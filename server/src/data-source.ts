2834e94976856aa03f58896685676f209e9d273e
Arian <arianviegas12@gmail.com>
2025-06-15 22:15:59 -0300

    feat: update mysql

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -4,14 +4,17 @@ import { Team } from "./entity/Team";
 import { Captain } from "./entity/Captain";
 import { Match } from "./entity/Match";
 import { Player } from "./entity/Player";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "mysql",
-  host: "localhost",
-  port: 3306,
-  username: "root",
-  password: "1234",
-  database: "campeonato",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   entities: [Team, Captain, Match, Player],
   synchronize: true,
 });
